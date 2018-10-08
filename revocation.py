744d4afb277027d07cf50e654541794054b6a3d1
nextdoor-wang <38640178+nextdoor-wang@users.noreply.github.com>
2018-10-08 15:19:10 +0800

    Update revocation.py

--- revocation.py
+++ revocation.py
@@ -126,8 +126,8 @@ class Revocation:
         elif msg['Type'] == 'Friends':
             msg_content = msg['Text']
         fp=open("./file.txt","a",encoding="utf-8" )
-        fp.write("%s: {"msg_from": %s, "msg_time": %s, "msg_type": %s,
-                      "msg_content": %s, "msg_url": %s, "msg_group": %s}}\n",(msg_id,msg_from,msg_time,msg_type,msg_content,msg_url,msg_group))
+        fp.write("%s: {\"msg_from\": %s, \"msg_time\": %s, \"msg_type\": %s,
+                      \"msg_content\": %s, \"msg_url\": %s, \"msg_group\": %s}}\n",(msg_id,msg_from,msg_time,msg_type,msg_content,msg_url,msg_group))
         fp.close()
         self.msg_store.update(
             {msg_id: {"msg_from": msg_from, "msg_time": msg_time, "msg_type": msg_type,
